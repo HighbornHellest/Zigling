abb0b4e835901e4e4f24f2e4bd9a5103dc160413
HighbornHellest <15896727+HighbornHellest@users.noreply.github.com>
2026-07-21 08:25:58 +0200

    the right way

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -14,13 +14,13 @@ pub fn main(init: std.process.Init) !void {
     var stdout_writer = std.Io.File.stdout().writer(io, &.{});
     const stdout = &stdout_writer.interface;
 
-    //const my_num: u32 = getNumber();
+    const my_num: u32 = getNumber() catch 42;
 
-    try stdout.print("my_num={}\n", .{42});
+    try stdout.print("my_num={}\n", .{my_num});
 }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
-// fn getNumber() NumError!u32 {
-//     if (false) return NumError.IllegalNumber;
-//     return 42;
-// }
+fn getNumber() NumError!u32 {
+    if (false) return NumError.IllegalNumber;
+    return 42;
+}
